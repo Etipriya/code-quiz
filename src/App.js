d784de19f1bdf16bc4820580f678fdc136fffbc4
Eti Priya <etipriya@gmail.com>
2023-03-07 01:25:30 +0000

    add state for start container

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./App.css";
 class App extends Component {
   constructor(prop) {
     super(prop);
+
+    this.state = {
+      showStartContainer: true,
+    };
   }
 
   startQuiz = () => {
@@ -17,11 +21,13 @@ class App extends Component {
           <h1>CODE QUIZ</h1>
         </div>
 
-        <div className="position-absolute top-50 start-50 translate-middle start-quiz-container">
-          <button className="btn btn-success" onClick={this.startQuiz}>
-            START
-          </button>
-        </div>
+        {this.state.showStartContainer && (
+          <div className="position-absolute top-50 start-50 translate-middle start-quiz-container">
+            <button className="btn btn-success" onClick={this.startQuiz}>
+              START
+            </button>
+          </div>
+        )}
       </div>
     );
   }
